5217537ae80b298d11763dd7846e9029f3a5823f
Denis Slavetskiy <72410578+slavetskiy@users.noreply.github.com>
2020-11-11 00:31:52 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,41 +3,40 @@ import kotlin.math.*
 fun main() {
     // Будем вводить начальные данные руками
     println("Введите сумму предыдущих покупок")
-    val inputTotalAmount = readLine()!!.toInt()
+    val totalAmount = readLine()!!.toInt()
 
     println("Были у вас покупки за последний месяц\n" +
             "1 - Да\n" +
             "Любой другой символ - Нет")
-    val inputIsUserMeloman = when (readLine()) {
+    val isUserMeloman = when (readLine()) {
         "1" -> true
         else -> false
     }
 
     println("Введите сумму текущей покупки без скидки")
-    val inputAmountWithoutDiscount = readLine()!!.toInt()
+    val amountWithoutDiscount = readLine()!!.toInt()
 
-    println(discountedAmount(inputTotalAmount, inputIsUserMeloman, inputAmountWithoutDiscount))
+    val discountedAmount: Int
 
-}
-
-fun discountedAmount(totalAmount: Int = 0, isUserMeloman: Boolean = false, amountWithoutDiscount: Int): Int {
-
-    val intermediateCalculations: Int
-
-    if (amountWithoutDiscount > 10000) {
-        val intermediateCalculations: Int = round(0.95 * amountWithoutDiscount).toInt()
+    if (totalAmount > 10000) {
+        discountedAmount = round(amountWithoutDiscount * 0.95).toInt()
     }
-    else if (amountWithoutDiscount > 1000) {
-                val intermediateCalculations: Int = amountWithoutDiscount - 100
-            }
-        else {
-        val intermediateCalculations: Int = amountWithoutDiscount
+    else if (totalAmount > 1000) {
+        discountedAmount = amountWithoutDiscount - 100
+    }
+    else {
+        discountedAmount = amountWithoutDiscount
     }
 
-    return when {
-        isUserMeloman -> round(intermediateCalculations * 0.99).toInt()
-        else -> intermediateCalculations
+    val finalPrice: Int
 
+    if (isUserMeloman) {
+        finalPrice = (round(discountedAmount * 0.99)).toInt()
     }
+    else {
+        finalPrice = discountedAmount
+    }
+
+    println("Для вас будет цена $finalPrice")
 
 }
